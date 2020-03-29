6cb445ce34f8c9c3ffec6f800c95f676058819f9
Fernando Lima Borrelli <flborrelli@gmail.com>
2020-03-29 13:32:10 -0300

    changing port

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const app = require('./app');
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log(`Running on PORT: ${process.env.PORT}`)
 });
