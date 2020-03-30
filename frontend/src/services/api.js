01552e9e1ae4590366a38780332935bf6ddf4d02
Fernando Lima Borrelli <flborrelli@gmail.com>
2020-03-30 17:26:59 -0300

    changing env

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 const localUrl = 'http://localhost:5000';
 
 const api = axios.create({
-  baseURL: localUrl || 'https://be-the-hero-full-app.herokuapp.com',
+  baseURL: 'https://be-the-hero-full-app.herokuapp.com',
 })
 
 export default api;
