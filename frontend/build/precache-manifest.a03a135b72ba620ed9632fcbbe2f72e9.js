264326fa4665485fa170279a2216d769282cbce3
Fernando Lima Borrelli <flborrelli@gmail.com>
2020-03-30 17:16:39 -0300

    change env.

---
+++ frontend/build/precache-manifest.a03a135b72ba620ed9632fcbbe2f72e9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39b08d66dd87aabb74e7816dea50e4ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f49b284fc31fc3931ee6",
+    "url": "/static/css/2.f42e59c9.chunk.css"
+  },
+  {
+    "revision": "a4c182038e780733784b",
+    "url": "/static/css/main.9d4f8182.chunk.css"
+  },
+  {
+    "revision": "f49b284fc31fc3931ee6",
+    "url": "/static/js/2.bcbc73fa.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.bcbc73fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4c182038e780733784b",
+    "url": "/static/js/main.8412406f.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  },
+  {
+    "revision": "1f4b5508873e87c04ab679e647a7954b",
+    "url": "/static/media/heroes.1f4b5508.png"
+  },
+  {
+    "revision": "7eea718b48893e461f82049ec46c1e1c",
+    "url": "/static/media/logo.7eea718b.svg"
+  }
+]);
